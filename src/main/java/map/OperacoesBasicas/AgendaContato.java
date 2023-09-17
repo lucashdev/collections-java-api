60685a7a9c349fdf73c658681284332d984c778a
Lucas Henrique <lucasthdf@gmail.com>
2023-09-17 19:47:04 -0300

    Ver 5

---
+++ src/main/java/map/OperacoesBasicas/AgendaContato.java
@@ -0,0 +1,34 @@
+package main.java.map.OperacoesBasicas;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AgendaContato{
+    private Map<String, Integer> agendaContatoMap;
+
+    public AgendaContato() {
+        this.agendaContatoMap = new HashMap<>();
+    }
+
+    public void adicionarContato(String nome, Integer telefone){
+        agendaContatoMap.put(nome, telefone);
+    }
+
+    public Void removerContato(String nome) {
+        if (!agendaContatoMap.isEmpty()) {
+            agendaContatoMap.remove(nome);
+        }
+    }
+
+    public void exibirContatos(){
+        System.out.println(agendaContatoMap);
+    }
+
+    public Integer pesquisarPorNome(String nome) {
+        Integer numeroPorNome =null;
+        if (!agendaContatoMap.isEmpty()){
+           numeroPorNome = agendaContatoMap.get(nome);
+        }
+        return numeroPorNome;
+    }
+}
